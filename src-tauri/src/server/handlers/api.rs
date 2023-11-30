0cf024c2c639dcadb15df3b7688dc383c4aa8fb6
Adam Jaworski <savander.pl@gmail.com>
2023-11-30 16:05:13 +0100

    feat: more specific requestform for logs

--- src-tauri/src/server/handlers/api.rs
+++ src-tauri/src/server/handlers/api.rs
@@ -4,33 +4,44 @@ use tauri::Manager;
 
 use crate::server::TauriAppState;
 
-#[derive(Serialize, Deserialize)]
-struct LogsRequest {
-    message: String,
+#[derive(Clone, Serialize, Deserialize)]
+struct Origin {
+    function_name: String,
+    file: String,
+    line_number: u32,
+    hostname: String,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+struct Payloads {
+    #[serde(rename = "type")]
+    log_type: String,
+    content: serde_json::Value,
+    origin: Origin,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+struct LogsFormRequest {
+    uuid: String,
+    payloads: Vec<Payloads>,
+    meta: serde_json::Value,
 }
 
 #[derive(Clone, Serialize)]
 struct LogsResponse {
-    message: String,
+    uuid: String,
 }
 
 #[post("/api/logs")]
 pub async fn post_logs_handle(
     data: web::Data<TauriAppState>,
-    logs: web::Json<LogsRequest>,
+    logs: web::Json<LogsFormRequest>,
 ) -> impl Responder {
     let tauri_app = &data.app.lock().unwrap();
 
-    tauri_app
-        .emit_all(
-            "post-logs",
-            LogsResponse {
-                message: logs.message.clone(),
-            },
-        )
-        .unwrap();
+    tauri_app.emit_all("post-logs", logs.clone()).unwrap();
 
     HttpResponse::Ok().json(LogsResponse {
-        message: logs.message.clone(),
+        uuid: logs.uuid.clone(),
     })
 }
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -3,19 +3,26 @@ import { listen } from '@tauri-apps/api/event';
 import { onMounted, ref } from 'vue';
 
 interface Log {
-  id: number;
-  message: string;
+  uuid: string;
+  payloads: {
+    type: string;
+    content: any;
+    origin: {
+      function_name: string,
+      file: string,
+      line_number: number,
+      hostname: string,
+    };
+  }[];
+  meta: any;
 }
 
 const logs = ref<Log[]>([]);
 
 onMounted(() => {
   listen('post-logs', (event) => {
-    const payload = event.payload as { message: string };
-    logs.value.push({
-      id: Date.now(),
-      message: payload.message
-    });
+    const payload = event.payload as Log;
+    logs.value.push(payload);
   });
 });
 </script>
@@ -24,10 +31,43 @@ onMounted(() => {
   <div>
     <h1>Home</h1>
 
-    <ul>
-      <li v-for="log in logs" :key="log.id">
-        {{ log.message }}
-      </li>
-    </ul>
+    <TransitionGroup name="list" tag="div" class="p-4 flex flex-col gap-4">
+      <div v-for="log in logs" :key="log.uuid" class="p-4 bg-slate-200 rounded border border-slate-300">
+        <div v-for="payload in log.payloads" class="bg-slate-100 p-4 mb-2 rounded hover:bg-slate-300 transition-colors">
+          <div class="flex items-center justify-between mb-3">
+            <span class="text-xs font-mono text-slate-500">
+              {{ payload.origin.function_name }} ({{ payload.origin.file }}:{{ payload.origin.line_number }})
+            </span>
+
+            <span class="text-xs font-mono text-slate-500 ml-2 font-bold">
+              {{ payload.type }}
+            </span>
+          </div>
+
+          <div class="text-sm text-slate-900 mb-3">
+            {{ payload.content }}
+          </div>
+        </div>
+
+        <hr class="border-t border-slate-300 mb-3 mt-5">
+
+        <span class="text-xs font-mono text-slate-500">
+          Uuid: {{ log.uuid }}
+        </span>
+      </div>
+    </TransitionGroup>
   </div>
 </template>
+
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.2s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
